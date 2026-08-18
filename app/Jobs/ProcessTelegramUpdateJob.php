61988396b76518e480990a32403ff2c4baa0e427
Mhoseinshah1 <mhoseinshahpoori2005@gmail.com>
2026-08-18 21:48:27 +0330

    fix: release Telegram update idempotency after exhausted retries

--- app/Jobs/ProcessTelegramUpdateJob.php
+++ app/Jobs/ProcessTelegramUpdateJob.php
@@ -8,6 +8,8 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Cache;
+use Throwable;
 
 class ProcessTelegramUpdateJob implements ShouldQueue
 {
@@ -28,5 +30,12 @@ class ProcessTelegramUpdateJob implements ShouldQueue
     public function handle(TelegramUpdateRouter $router): void
     {
         $router->handle($this->update);
+    }
+
+    public function failed(Throwable $exception): void
+    {
+        if (isset($this->update['update_id'])) {
+            Cache::forget('tg-update:'.$this->update['update_id']);
+        }
     }
 }
